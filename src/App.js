3267e194fdfec8784cec68f2bb5dbcae22984d60
Rahul kumar dhar dubey <78297145+rkdd51@users.noreply.github.com>
2022-12-24 17:01:05 +0530

    revisionFirstCommit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Counter from "./features/counter/Counter";
+function App() {
+  return (
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
